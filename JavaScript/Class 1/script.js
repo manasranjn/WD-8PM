361e026c259a34cdccebac8fe81ad924f5e6eb1b
Manas Ranjan Bera <manasbera484@gmail.com>
2025-09-03 21:04:20 +0530

    JS

---
+++ JavaScript/Class 1/script.js
@@ -0,0 +1,78 @@
+// console.log("Hello Everyone");
+
+//! Variable Keywords
+//? var
+var v
+v = 0
+// var a = 10
+// console.log(a);
+
+// var a = 20
+// console.log(a);
+
+// a = "hi"
+// console.log(a);
+
+//? let
+let l
+l = 1
+let b = "Hi"
+// console.log(b);
+
+// let b //not allowed
+// b = "Hello"
+// console.log(b);
+// b = 10
+// console.log(b);
+
+//? const- constant
+const c = 10
+// const c //not allowed
+// c = 20 //not allowed
+// console.log(c);
+
+//! Data Types
+//? Primitive Type
+//* Number
+let n1 = 10
+let n2 = -10
+let n3 = 1.5
+// console.log(typeof n1);
+// console.log(typeof n2);
+// console.log(typeof n3);
+
+//* boolean
+let x = true
+let y = false
+// console.log(typeof (x));
+// console.log(typeof y);
+
+//* String
+let s1 = 'Hello'
+let s2 = "Hi"
+let s3 = "12345678"
+let s4 = '!@#$%^&*'
+// console.log(typeof s1);
+// console.log(typeof s2);
+// console.log(typeof s3);
+// console.log(typeof s4);
+
+//* undefined
+let u
+// console.log(u);
+// console.log(typeof u);
+
+//* null
+let p = null
+// console.log(p);
+// console.log(typeof p);
+
+//* Symbol
+let sym = Symbol(123)
+// console.log(sym);
+// console.log(typeof sym);
+
+//* BigInt
+let big = BigInt(999999999000000)
+// console.log(big);
+// console.log(typeof big);
