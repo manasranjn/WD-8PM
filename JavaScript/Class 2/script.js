1831e286ae1eeea0c816504b114db12d1f1c7de9
Manas Ranjan Bera <manasbera484@gmail.com>
2025-09-04 20:50:28 +0530

    Operator

---
+++ JavaScript/Class 2/script.js
@@ -0,0 +1,94 @@
+//! Non Primitive Type
+//? Array
+let arr = [1, 2, 3, true, undefined, "Hi"]
+// console.log(arr);
+// console.log(arr[3]);
+
+//? Object
+let obj = {
+    name: "ABCD",
+    age: 20,
+    pass: true
+}
+// console.log(obj);
+// console.log(obj['age']);
+// console.log(obj.name);
+
+//? Function
+function greet() {
+    console.log("This is a function");
+    console.log("Hello Everyone");
+}
+// greet()
+// greet()
+
+//! Operators
+//? Arithmetic Operator
+let a = 20
+let b = 3
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+//? Assignment Operator
+a = 50
+// console.log(a += 10) // a = a+ 10
+// console.log(a -= 20); // a= a-20
+// console.log(a *= 2);
+// console.log(a /= 2);
+// console.log(a **= 2);
+// console.log(a %= 3);
+
+//? Comparision Operator
+let p = 10
+let q = 20
+let r = "10"
+let s = 10
+// console.log(p == q);
+// console.log(p == r);
+// console.log(p != q);
+// console.log(p === q);
+// console.log(p === r);
+// console.log(p !== q);
+// console.log(p < q);
+// console.log(p > q);
+// console.log(p > s);
+// console.log(p >= s);
+// console.log(p <= s);
+
+//? Logical Operator
+let x = true
+let y = false
+let z = true
+//* AND(Multiply)
+// console.log(x && y);
+// console.log(x && z);
+//* OR(Addition)
+// console.log(x || y);
+// console.log(x || z);
+//* NOT(Reverse)
+// console.log(!x);
+// console.log(!y);
+
+//? Unary Operator
+a = 10
+//* Increment
+// console.log(++a); //pre-increment
+// console.log(a++); //post-increment
+// console.log(a);
+// console.log(++a);
+// console.log(--a);
+// console.log(++a);
+// console.log(a); //13
+// console.log(++a);
+// console.log(a++);
+// console.log(a--);
+// console.log(a); //16
+
+//* Decrement
+// console.log(--a);
+// console.log(a--);
+// console.log(a);
