ba7a306ee5abce167c5c33fbd01946e7c578de6c
Manas Ranjan Bera <manasbera484@gmail.com>
2025-09-05 20:40:11 +0530

    init

---
+++ JavaScript/Class 3/script.js
@@ -0,0 +1,41 @@
+//! Conditional Statement
+//? if statement
+let a = 15
+// if (a > 15) {
+//     console.log("Hi");
+// }
+
+//? if-else
+// if (a >= 15) {
+//     console.log("If Block");
+// } else {
+//     console.log("Else Block");
+// }
+
+//? else-if
+a = 35
+// if (a > 20 && a < 30) {
+//     console.log("1st block");
+// } else if (a > 30 && a < 40) {
+//     console.log("2nd block");
+// } else if (a > 40 && a < 50) {
+//     console.log("3rd Block");
+// } else {
+//     console.log("Else Block");
+// }
+
+//? Switch Case
+let value = 2
+switch (value) {
+    case 1:
+        console.log("A");
+        break
+    case 2:
+        console.log("B");
+        break
+    case 3:
+        console.log("C");
+        break
+    default:
+        console.log("D");
+}
