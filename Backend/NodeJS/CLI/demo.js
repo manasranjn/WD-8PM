e101f6e5f3aa95ea22fd273f140257ff6d2ff7a2
Manas Ranjan Bera <manasbera484@gmail.com>
2025-12-11 21:06:33 +0530

    file system

---
+++ Backend/NodeJS/CLI/demo.js
@@ -0,0 +1,17 @@
+// console.log("Hello Everyone");
+
+function greet(name) {
+    console.log(`Hello ${name}`);
+
+}
+// greet()
+
+console.log(process.argv);
+
+// greet(process.argv[2])
+
+const arg = process.argv[2]
+console.log(arg);
+
+
+arr = [1, "hi", " ", undefined]
