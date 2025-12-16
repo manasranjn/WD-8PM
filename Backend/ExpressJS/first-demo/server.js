90782d8f7dcb46bd11682cb01467e9437d539808
Manas Ranjan Bera <manasbera484@gmail.com>
2025-12-16 21:02:29 +0530

    Routes

--- Backend/ExpressJS/first-demo/server.js
+++ Backend/ExpressJS/first-demo/server.js
@@ -3,9 +3,21 @@ const app = express()
 const PORT = 5000
 
 app.get('/', (req, res) => {
-    console.log(req.method);
 
     res.send('Hello World!')
+})
+
+app.get("/books", (req, res) => {
+    res.send("normal route")
+})
+app.get("/books/:id", (req, res) => {
+    res.send(`book id is ${req.params.id}`)
+})
+
+app.get('/search', (req, res) => {
+    console.log(req.query);
+
+    res.send(`Search for: ${req.query.price} and ${req.query.rating} and ${req.query.color}`)
 })
 
 app.listen(PORT, () => {
