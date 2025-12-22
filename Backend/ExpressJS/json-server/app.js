e90f2c98d03caa3dd2c988a4d6844050ddb69177
Manas Ranjan Bera <manasbera484@gmail.com>
2025-12-22 21:29:12 +0530

    middleware

--- Backend/ExpressJS/json-server/app.js
+++ Backend/ExpressJS/json-server/app.js
@@ -3,11 +3,19 @@ const app = express();
 const PORT = 8000;
 
 app.get('/', (req, res) => {
-    res.send({ message: 'Hello, World!' });
+    res.send({
+        message: 'Hello, World!',
+        name: 'John Doe',
+        age: 30,
+        city: 'New York'
+    });
+    // console.log(req.method);
+
 });
 
 app.post('/', (req, res) => {
     res.send({ message: 'POST request received!' });
+    // console.log(req.method);
 });
 
 app.listen(PORT, () => {
