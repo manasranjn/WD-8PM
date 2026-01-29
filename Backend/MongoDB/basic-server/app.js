6e6510271ef9d841bd0eeab3b18eb8d60bf0c817
Manas Ranjan Bera <manasbera484@gmail.com>
2026-01-29 21:04:33 +0530

    schema

--- Backend/MongoDB/basic-server/app.js
+++ Backend/MongoDB/basic-server/app.js
@@ -8,6 +8,26 @@ const PORT = 3500
 // const URI = process.env.MONGO_URI
 // console.log(URI);
 
+const studentSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    course: String,
+    section: String,
+    mark: Number
+})
+
+const Student = mongoose.model('Student', studentSchema)
+
+const s1 = new Student({
+    name: 'Gyana',
+    age: 22,
+    course: "MCA",
+    section: 'A',
+    mark: 425
+})
+
+s1.save()
+
 mongoose.connect(process.env.MONGO_URI)
     .then((res) => {
         console.log("Server Connected");
